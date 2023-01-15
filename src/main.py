63d75548e5256c2936d1a666f8401e8d811684b1
mitrandir-pl <leshkopavel@gmail.com>
2023-01-15 12:48:32 +0300

    db init added

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 import os
 
 from fastapi import FastAPI
-from sqlmodel import create_engine, SQLModel, Session, Field
+from sqlmodel import create_engine, SQLModel
 from dotenv import load_dotenv
 
-from src.tables.user import init_users_table
-from src.tables.post import init_posts_table
+from tables.user import init_users_table
+from tables.post import init_posts_table
+from routes import register
 
 load_dotenv()
 app = FastAPI()
-
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
@@ -17,6 +17,7 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
+
 
 @app.get("/")
 async def home():
